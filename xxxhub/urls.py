963c9e6d6da25b2044ce9835890224be76cb9361
Denis <sersdens@gmail.com>
2022-11-08 22:03:12 +0500

    1

--- xxxhub/urls.py
+++ xxxhub/urls.py
@@ -19,6 +19,6 @@ from django.urls import include
 
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
